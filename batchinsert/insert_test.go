d1066fdfeb0aaa190d23719992f0ef22278323cc
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-11-11 13:41:46 +0700

    feat: add concurrent insert benchmark

--- batchinsert/insert_test.go
+++ batchinsert/insert_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/driver/postgres"
@@ -35,10 +36,14 @@ func BenchmarkInsertSingle(b *testing.B) {
 		db.Where("id is not null").Delete(&Employee{})
 		b.StartTimer()
 		for _, d := range datas {
-			_ = db.Create(&d)
+			tx := db.Create(&d)
+			if tx.Error != nil {
+				log.Println(tx.Error)
+			}
 		}
 
 	}
+	db.Where("id is not null").Delete(&Employee{})
 
 }
 
@@ -71,5 +76,43 @@ func BenchmarkInsertBatch(b *testing.B) {
 		_ = db.Create(&datas)
 
 	}
+	db.Where("id is not null").Delete(&Employee{})
+
+}
+func BenchmarkInsertGoroutine(b *testing.B) {
+	datas := []Employee{}
+	n := 10000
+	for range n {
+		emp := Employee{
+			NIP:          uuid.NewString(),
+			Name:         "some name",
+			OrgUnit:      "org unit",
+			EmployeeType: "employee type",
+			Status:       "status",
+			IsActive:     true,
+		}
+		datas = append(datas, emp)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		dsn := "host=localhost user=postgres password=postgres dbname=malut port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		sqlDB, _ := db.DB()
+		gcnt := 5
+		sqlDB.SetMaxIdleConns(gcnt)
+		sqlDB.SetMaxOpenConns(gcnt)
+		sqlDB.SetConnMaxLifetime(time.Hour)
+		db.Where("id is not null").Delete(&Employee{})
+		b.StartTimer()
+
+		insertConcurrent(db, datas, gcnt)
+		sqlDB.Close()
+	}
+	// db.Where("id is not null").Delete(&Employee{})
 
 }
--- batchinsert/main.go
+++ batchinsert/main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func singleInsert(db *gorm.DB, data []*Employee) {
-	for _, d := range data {
-		db.Create(d)
+func insertConcurrent(db *gorm.DB, data []Employee, cnt int) {
+	var wg sync.WaitGroup
+
+	wg.Add(cnt)
+	fmt.Println(len(data))
+	var chs []chan Employee
+	for range cnt {
+		c := make(chan Employee, len(data)/cnt)
+		chs = append(chs, c)
 	}
+	for i, e := range data {
+		idx := i % cnt
+		chs[idx] <- e
+	}
+	for i := range cnt {
+		close(chs[i])
+	}
+	for i := range cnt {
+		go func(ch <-chan Employee, dbs *gorm.DB) {
+			defer wg.Done()
+			session := dbs.Session(&gorm.Session{NewDB: true})
+			affectedRows := 0
+			for e := range ch {
+				res := session.Create(&e)
+				if res.Error != nil {
+					log.Println(res.Error)
+					continue
+				}
+				affectedRows++
+			}
+			fmt.Println("go routine no: ", i, affectedRows)
+		}(chs[i], db)
+	}
+	wg.Wait()
+
 }
 func main() {
 
@@ -17,7 +53,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	err = db.AutoMigrate(&Employee{})
+
 	if err != nil {
 		panic(err)
 	}
